5eca49284d11613beec68d81a28c294c37a57b59
shimla1501 <91822830+shimla1501@users.noreply.github.com>
2021-10-03 19:57:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # Version 1.1
-
 print("CEO Entrepreneur")
-
 print("Born in 1964")
-
 print("Jeffrey")
-
 print("Jeffrey Bezos")
+print("C'mon Jeffrey, you can do it.")
+print("Pave the way, put your back into it.")
